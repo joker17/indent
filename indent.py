db8a66312361dc6420ae0c3db634ab5bc2bd68e9
unknown <hejiang18696@hs.handsome.com.cn>
2017-07-07 20:58:57 +0800

    update

--- indent.py
+++ indent.py
@@ -1,10 +1,34 @@
 #!/usr/local/bin/python3
 
-file = open("data.txt")
+class Stack:
+  """模拟栈"""
+  def __init__(self):
+    self.items = []
+     
+  def isEmpty(self):
+    return len(self.items)==0
+   
+  def push(self, item):
+    self.items.append(item)
+   
+  def pop(self):
+    return self.items.pop() 
+   
+  def peek(self):
+    if not self.isEmpty():
+      return self.items[len(self.items)-1]
+     
+  def size(self):
+    return len(self.items) 
+
+symbol_stack = Stack()
+
+file = open("data.txt", 'r', encoding='UTF-8')
 lines = file.readlines()
 
 for line in lines:
-    print line
+    print(line)
 
-print lines[1]
+
+print(lines[1])
 file.close()
