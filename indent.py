6d02cae0434d73d6ab76d1730526cc43808cef5f
hj.sinp <openout5@qq.com>
2017-10-03 14:49:16 +0800

    merger two modle

--- indent.py
+++ indent.py
@@ -27,54 +27,65 @@ class Stack:
     return len(self.items) 
 
   def proc(self, char, num):
-    print("match,proc", char, num)
+    #print("match,proc", char, num)
     tmp_item = self.peek()
     if len(tmp_item) > 0 and [tmp_item[0], char] in match_symbols:
-      print("match,out", tmp_item[0], char)
+      #print("match,out", tmp_item[0], char)
       self.pop()
       if num != tmp_item[1]:
         return tmp_item[1]
       else:
         return -1
     else:
-      print("match,in", char, num)
+      #print("match,in", char, num)
       self.push([char, num])
       return -1
+
+def proc_indent(lines):
+    i = 0
+    #list ts_list
+    ts_list = list()
+    symbol_stack = Stack()
+
+    for line in lines:
+        print("==%s", line)
+        ts_list.append(0)
+        j = 0
+        for word in line:
+            if word in symbols:
+                match_line = symbol_stack.proc(word, i)
+                #print("return match_line", match_line, i)
+                if match_line == -1:
+                    continue
+                for k in range(match_line+1, i):
+                    #print("add to ts_list", k)
+                    ts_list[k] += 1
+            j += 1
+        i += 1
+
+    n = 0
+    alltext = []
+    newline = list()
+    for line in lines:
+        space_str = ""
+        cnt = ts_list[n]
+        #print("xxxxx = %d,%d", n, cnt)
+        for m in range(cnt * tabsize):
+            #print("===%d", n)
+            space_str = " " + space_str
+        newline = "%s%s" % (space_str, line.lstrip())
+        if (len(alltext) == 0):
+            alltext = "%s\n" % (newline)
+        else:
+            alltext = "%s%s\n" % (alltext, newline)
+        print(newline)
+        n += 1
+    return alltext
     
+
 #file = open("data.txt", 'r', encoding='UTF-8')
-file = open("data.txt", 'r')
-lines = file.readlines()
-
-i = 0
-#list ts_list
-ts_list = list()
-symbol_stack = Stack()
-
-for line in lines:
-    ts_list.append(0)
-    j = 0
-    for word in line:
-        if word in symbols:
-            match_line = symbol_stack.proc(word, i)
-            #print("return match_line", match_line, i)
-            if match_line == -1:
-                continue
-            for k in range(match_line+1, i):
-                #print("add to ts_list", k)
-                ts_list[k] += 1
-        j += 1
-    i += 1
-
-n = 0
-newline = list()
-for line in lines:
-    space_str = ""
-    cnt = ts_list[n]
-    #print("xxxxx = %d,%d", n, cnt)
-    for m in range(cnt * tabsize):
-        #print("===%d", n)
-        space_str = " " + space_str
-    newline = "%s%s" % (space_str, line.lstrip())
-    print(newline)
-    n += 1
-file.close()
+##file = open("data.txt", 'r')
+#lines = file.readlines()
+#all = proc_indent(lines)
+#print(all)
+#file.close()
